63cbe836b9f52bfd7ba336b38897ef90fd2a19f5
hakan çakır <baranc3363@gmail.com>
2022-02-25 06:13:19 +0300

    Add project files.

---
+++ AsenkronWindowsForms/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace AsenkronWindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOku = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnCounter = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnOku
+            // 
+            this.btnOku.Location = new System.Drawing.Point(12, 12);
+            this.btnOku.Name = "btnOku";
+            this.btnOku.Size = new System.Drawing.Size(120, 50);
+            this.btnOku.TabIndex = 0;
+            this.btnOku.Text = "Oku";
+            this.btnOku.UseVisualStyleBackColor = true;
+            this.btnOku.Click += new System.EventHandler(this.btnOku_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 68);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(217, 212);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // btnCounter
+            // 
+            this.btnCounter.Location = new System.Drawing.Point(308, 20);
+            this.btnCounter.Name = "btnCounter";
+            this.btnCounter.Size = new System.Drawing.Size(103, 35);
+            this.btnCounter.TabIndex = 2;
+            this.btnCounter.Text = "Sayaç ARttır";
+            this.btnCounter.UseVisualStyleBackColor = true;
+            this.btnCounter.Click += new System.EventHandler(this.btnCounter_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(313, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(118, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCounter);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnOku);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOku;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnCounter;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ AsenkronWindowsForms/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsenkronWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnOku_Click(object sender, EventArgs e)
+        {
+           string data= await ReadFileAsync();
+            richTextBox1.Text = data;
+        }
+
+
+        int Sayac = 0;
+        private void btnCounter_Click(object sender, EventArgs e)
+        {
+            Sayac++;
+            textBox1.Text = Sayac.ToString();
+
+        }
+
+
+        private string ReadFile()
+        {
+            string path = "dosya.txt";
+            string Data = string.Empty;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                Data = sr.ReadToEnd();
+            }
+
+            return Data;
+        }
+
+        private async Task<string> ReadFileAsync()
+        {
+            string path = "dosya.txt";
+            string Data = string.Empty;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                Task<string> myData = sr.ReadToEndAsync();
+
+                // Burda dataya bağlı olmiyan işlemlerimizi yaparız şuanda data çekiliyor
+                // metot asenkron olduğundan o işlemini yaparken biz farklı işlemler yaparız 
+
+                Data = await myData;// bu data artık bana lazım bunu bana verene kadar bekle
+           
+            return Data;
+            }
+        }
+    }
+
+
+
+}
