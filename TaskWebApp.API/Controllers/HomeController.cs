64e6a46eb61552623bfbf7ece15d15d27e8546a0
hakan çakır <baranc3363@gmail.com>
2022-02-27 00:56:59 +0300

    27.02Api

---
+++ TaskWebApp.API/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskWebApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetContentAsync()
+        {
+            // verilen linkteki bilgileri getirir
+            var myTask = new HttpClient().GetStringAsync("https://www.google.com");
+
+
+            var data = await myTask;
+
+            return Ok(data);
+        }
+    }
+}
