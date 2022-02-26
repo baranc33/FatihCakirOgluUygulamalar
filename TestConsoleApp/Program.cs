271620905d97e0ea2a90130d8151f04b835b8e0c
hakan çakır <baranc3363@gmail.com>
2022-02-27 02:38:45 +0300

    ContunieWithConsole

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Console.WriteLine("");
+
+Task myTask = new HttpClient().GetStringAsync("https://www.google.com")
+    .ContinueWith((data) =>
+    {
+        Console.WriteLine(data.Result.ToString());
+    });
+
+
+await myTask;
+
+
+
+
+static class test
+{
+    public static void MyMetot(string Data)
+    {
+        Console.WriteLine(Data);
+    }
+    public static void MyMetot2(string Data)
+    {
+        Console.WriteLine(Data.Length);
+    }
+}
+
+
