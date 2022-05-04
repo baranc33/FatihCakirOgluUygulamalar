a24dea4483b177bfd7a3bb550175fe75a41ab25d
Hakan Baran Çakir <baranc3363@gmail.com>
2022-05-05 02:36:24 +0300

    configuration and seed data

--- NLayerApp.Repository/AppDbContext.cs
+++ NLayerApp.Repository/AppDbContext.cs
@@ -20,5 +20,39 @@ namespace NLayerApp.Repository
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
 
-   }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            modelBuilder.Entity<ProductFeature>().HasData(new ProductFeature()
+            {
+                Id = 1,
+                Color = "Kırmızı",
+                Height = 100,
+                Width = 200,
+                ProductId = 1
+
+
+            },
+            new ProductFeature()
+            {
+                Id = 2,
+                Color = "Mavi",
+                Height = 300,
+                Width = 500,
+                ProductId = 2
+
+
+            }
+
+
+            );
+
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
 }
