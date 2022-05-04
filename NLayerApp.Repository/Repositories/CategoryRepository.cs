08b4a10f21977647bb6f45ad293583fa4268c2f4
Hakan Baran Çakir <baranc3363@gmail.com>
2022-05-05 02:44:49 +0300

    repository

---
+++ NLayerApp.Repository/Repositories/CategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace NLayerApp.Repository.Repositories
+{
+    //public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    //{
+    //    public CategoryRepository(AppDbContext context) : base(context)
+    //    {
+    //    }
+
+    //    public async Task<Category> GetSingleCategoryByIdWithProductsAsync(int categoryId)
+    //    {
+    //        return await _context.Categories.Include(x => x.Products).Where(x => x.Id == categoryId).SingleOrDefaultAsync();
+    //    }
+    //}
+    internal class CategoryRepository
+    {
+    }
+}
---
+++ NLayerApp.Repository/Repositories/ProductRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace NLayerApp.Repository.Repositories
+{
+
+    //public class ProductRepository : GenericRepository<Product>, IProductRepository
+    //{
+    //    public ProductRepository(AppDbContext context) : base(context)
+    //    {
+    //    }
+
+    //    public async Task<List<Product>> GetProductsWitCategory()
+    //    {
+
+    //        return await _context.Products.Include(x => x.Category).ToListAsync();
+    //    }
+    //}
+    internal class ProductRepository
+    {
+    }
+}
