dad994d911b3f650db5e11604fcb9f0a35f5e96a
Hakan Baran Çakir <baranc3363@gmail.com>
2022-05-05 03:01:26 +0300

    migration and program.cs

--- NLayerApp.Repository/Repositories/CategoryRepository.cs
+++ NLayerApp.Repository/Repositories/CategoryRepository.cs
@@ -1,17 +1,19 @@
-﻿namespace NLayerApp.Repository.Repositories
-{
-    //public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
-    //{
-    //    public CategoryRepository(AppDbContext context) : base(context)
-    //    {
-    //    }
+﻿using Microsoft.EntityFrameworkCore;
+using NLayerApp.Core.Entites;
+using NLayerApp.Core.Repositories;
 
-    //    public async Task<Category> GetSingleCategoryByIdWithProductsAsync(int categoryId)
-    //    {
-    //        return await _context.Categories.Include(x => x.Products).Where(x => x.Id == categoryId).SingleOrDefaultAsync();
-    //    }
-    //}
-    internal class CategoryRepository
+namespace NLayerApp.Repository.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
     {
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Category> GetSingleCategoryByIdWithProductsAsync(int categoryId)
+        {
+            return await _context.Categories.Include(x => x.Products).Where(x => x.Id == categoryId).SingleOrDefaultAsync();
+        }
     }
+
 }
--- NLayerApp.Repository/Repositories/ProductRepository.cs
+++ NLayerApp.Repository/Repositories/ProductRepository.cs
@@ -1,19 +1,21 @@
-﻿namespace NLayerApp.Repository.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using NLayerApp.Core.Entites;
+using NLayerApp.Core.Repositories;
+
+namespace NLayerApp.Repository.Repositories
 {
 
-    //public class ProductRepository : GenericRepository<Product>, IProductRepository
-    //{
-    //    public ProductRepository(AppDbContext context) : base(context)
-    //    {
-    //    }
-
-    //    public async Task<List<Product>> GetProductsWitCategory()
-    //    {
-
-    //        return await _context.Products.Include(x => x.Category).ToListAsync();
-    //    }
-    //}
-    internal class ProductRepository
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
     {
+        public ProductRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<List<Product>> GetProductsWitCategory()
+        {
+
+            return await _context.Products.Include(x => x.Category).ToListAsync();
+        }
     }
+
 }
