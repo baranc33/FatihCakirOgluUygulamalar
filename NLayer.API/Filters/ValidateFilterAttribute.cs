b32047015b3c4fba0a41e1a5f94b39cd3ea59936
Hakan Baran Çakir <baranc3363@gmail.com>
2022-05-10 01:19:45 +0300

    ValidateFilterAttribute

---
+++ NLayer.API/Filters/ValidateFilterAttribute.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using NLayerApp.Core.ResponseDto;
+
+namespace NLayer.API.Filters
+{
+    public class ValidateFilterAttribute : ActionFilterAttribute// atribute sınıfı
+    {
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                // model state içindeki hataları seçip mesajları alıyoruz
+                var errors = context.ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+                // BadRequestResult => boş döner biz object olanı kullancaz bodye hataları doldurcaz
+                context.Result = new BadRequestObjectResult(CustomResponseDto<NoContentDto>.Fail(400, errors));
+
+
+            }
+        }
+    }
+}
